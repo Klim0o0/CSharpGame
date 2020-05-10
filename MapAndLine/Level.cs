41cad2ed744ea59a62db2fb37e9d2566cccbc7ab
klim0o0 <kirill.klimmmm@gmail.com>
2020-05-10 16:50:23 +0500

    Level

--- MapAndLine/Level.cs
+++ MapAndLine/Level.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 namespace Game.MapAndLine
 {
-    public static  class Level
+    public static class Level
     {
         static string texture = "wall";
 
@@ -11,12 +11,23 @@ namespace Game.MapAndLine
         {
             [1] = new List<Wall>()
             {
-                new Wall(new Line(new Vector(500, 200), new Vector(500, 400)), texture),
-                new Wall(new Line(new Vector(600, 5), new Vector(600, 600)), texture),
-                new Wall(new Line(new Vector(600, 600), new Vector(5, 600)), texture),
-                new Wall(new Line(new Vector(5, 600), new Vector(5, 5)), texture),
-                new Wall(new Line(new Vector(5, 600), new Vector(5, 5)), texture),
-                new Wall(new Line(new Vector(5, 5), new Vector(600, 5)), texture)
+                new Wall(new Line(new Vector(600, 200), new Vector(5, 200)), texture),
+                new Wall(new Line(new Vector(2000, 5), new Vector(2000, 2000)), texture),
+                new Wall(new Line(new Vector(600, 2000), new Vector(600, 200)), texture),
+                new Wall(new Line(new Vector(2000, 2000), new Vector(5, 2000)), texture),
+                new Wall(new Line(new Vector(5, 2000), new Vector(5, 5)), texture),
+                new Wall(new Line(new Vector(2000, 300), new Vector(1500, 300)), texture),
+                new Wall(new Line(new Vector(1500, 300), new Vector(1500, 600)), texture),
+                new Wall(new Line(new Vector(1500, 600), new Vector(2000, 600)), texture),
+                new Wall(new Line(new Vector(5, 5), new Vector(2000, 5)), texture),
+                new Wall(new Line(new Vector(800, 2000), new Vector(800, 1200)), texture),
+                new Wall(new Line(new Vector(800, 1200), new Vector(2000, 1200)), texture),
+                new Wall(new Line(new Vector(1000, 400), new Vector(800, 500)), texture),
+                new Wall(new Line(new Vector(800, 500), new Vector(750, 750)), texture),
+                new Wall(new Line(new Vector(750, 750), new Vector(950, 850)), texture),
+                new Wall(new Line(new Vector(950, 850), new Vector(1150, 800)), texture),
+                new Wall(new Line(new Vector(1150, 800), new Vector(1150, 450)), texture),
+                new Wall(new Line(new Vector(1150, 450), new Vector(1000, 400)), texture)
             }
         };
     }
