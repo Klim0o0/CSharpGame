451cf01952c0dde098088457bed56ab86409b0b4
klim0o0 <kirill.klimmmm@gmail.com>
2020-05-10 14:55:12 +0500

    Normal draw

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace Game
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new MyForm());
+            //Application.Run(new Menu());
         }
     }
 }
